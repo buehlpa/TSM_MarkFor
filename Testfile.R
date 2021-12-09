c01d4690f98e710467202ca677aafb149735c862
pascal bühler <buehler1991@gmail.com>
2021-12-09 11:54:27 +0100

    Added inverse part

--- Testfile.R
+++ Testfile.R
@@ -1,69 +1,91 @@
+#Libraries
+
+library(GGally)
+library(readr)
+library(ggplot2)
+library("ggpubr")
+
+
+#read the data and plot it
 fish_dat=readr::read_csv("Fish.csv")                        #  reading as tibble
 GGally::ggpairs(fish_dat,aes(color = Species)) + theme_bw() #  pairwise plot 
 summary(fish_dat);str(fish_dat)  
 
 
 
+# showing the whole transformation of weight
+# fish_dat$Weight=sqrt(fish_dat$Weight)
+# GGally::ggpairs(fish_dat,aes(color = Species)) + theme_bw() #  pairwise plot 
 
+# Linear Regression####
+
+# Use only Perch data
 
 
 Perch_dat=fish_dat%>%dplyr::filter(Species=="Perch")
 
 
-dim(Perch_dat)
 
-# Use only Perch data
+#split test train
 
 #aprox 11 (80%)
 #test  datapoints
 #train  45 data points
-set.seed(42);samp= sample(x = 1:56,size = 11,replace = FALSE)
-Perch_dat=Perch_dat[-samp,]
 
-Perch_fit=lm(sqrt(Weight)~Width,data = Perch_train)
+dim(Perch_dat)
+set.seed(42)
+samp= sample(x = 1:56,size = 11,replace = FALSE)
+Perch_train=Perch_train[-samp,]
+Perch_test=Perch_dat_test[samp,]
 
-  
 
 
+# comparison of targetvariable weigth with transformations
 par(mfrow=c(1,3))
-plot(Weight~Width,data=Perch_dat)
-plot(1/Weight~Width,data=Perch_dat)
-plot(sqrt(Weight)~Width,data=Perch_dat)
+plot(Weight~Width,data=Perch_train)
+plot(log(Weight)~Width,data=Perch_train)
+plot(sqrt(Weight)~Width,data=Perch_train)
 
-library("ggpubr")
-library(ggplot2)
 
-nPlt=ggplot(Perch_dat,aes(Width,Weight))+geom_point()
-logPlt=ggplot(Perch_dat,aes(Width,log(Weight)))+geom_point()
-sqrtPlt=ggplot(Perch_dat,aes(Width,sqrt(Weight)))+geom_point()
+# a nicer version in ggplot
+nPlt=ggplot(Perch_train,aes(Width,Weight))+geom_point()
+logPlt=ggplot(Perch_train,aes(Width,log(Weight)))+geom_point()
+sqrtPlt=ggplot(Perch_train,aes(Width,sqrt(Weight)))+geom_point()
 figure <- ggarrange(nPlt, logPlt, sqrtPlt,labels = c("A", "B", "C"),ncol = 3, nrow = 1)
 
 
+#SLR 
+Perch_fit=lm(sqrt(Weight)~Width,data = Perch_train)
 
+#residual analysis  
 par(mfrow=c(1,4))
 plot(Perch_fit)
 
-Perch_fit=lm(sqrt(Weight)~.,data = Perch_dat[,-1]) #  linear Regression model
-
 AIC(Perch_fit)
-
-
-predict()
-
-library(car)
-
-summary(Perch_fit)
 car::vif(Perch_fit)
 drop1(Perch_fit)
-?drop1
 
 
 
 
 
 
+#Full Model
+Perch_fit_full=lm(sqrt(Weight)~.,data = Perch_train[,-1]) 
 
 
+
+#MAD
+mad(predict(Perch_fit_full,Perch_test)-sqrt(Perch_test$Weight))            # 0.4947488
+mad(predict(Perch_fit,Perch_test)-sqrt(Perch_test$Weight))                 # 0.4694862
+
+#MSE
+0.1*(sum((predict(Perch_fit_full,Perch_test)-sqrt(Perch_test$Weight))^2))  # 1.045417   #exactly here is the problem with mse
+0.1*(sum((predict(Perch_fit,Perch_test)-sqrt(Perch_test$Weight))^2))       # 1.539654
+
+
+
+#deccorelate d ata
 
 
 
